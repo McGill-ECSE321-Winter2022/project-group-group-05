10cffa1e0a867fc6f2cf5a9659e0e7352524c7f1
Jim <71361453+jimcv@users.noreply.github.com>
2022-02-15 17:31:52 -0500

    PurchaseRepository find methods

--- GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/dao/PurchaseRepository.java
+++ GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/dao/PurchaseRepository.java
@@ -1,9 +1,22 @@
 package mcgill.ecse321.grocerystore.dao;
 
+import java.util.ArrayList;
 import org.springframework.data.repository.CrudRepository;
 import mcgill.ecse321.grocerystore.model.Purchase;
+import mcgill.ecse321.grocerystore.model.Purchase.PurchaseState;
 
 public interface PurchaseRepository extends CrudRepository<Purchase, Long> {
 
+  Purchase findPurchaseById(long id);
+
+  ArrayList<Purchase> findByIsDelivery(boolean isDelivery);
+
+  ArrayList<Purchase> findByState(PurchaseState state);
+
+  ArrayList<Purchase> findByStateOrderByTimeOfPurchaseMillis(PurchaseState state);
+
+  ArrayList<Purchase> findByStateOrderByTimeOfPurchaseMillisDesc(PurchaseState state);
+
+  ArrayList<Purchase> findByTimeOfPurchaseMillisGreaterThanEqual(long time);
 
 }
