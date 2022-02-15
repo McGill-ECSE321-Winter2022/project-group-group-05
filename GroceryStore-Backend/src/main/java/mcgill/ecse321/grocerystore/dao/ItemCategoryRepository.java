763029daf961a40c5e61224f803ed72095210554
Jim <71361453+jimcv@users.noreply.github.com>
2022-02-15 04:12:49 -0500

    Revert "Remove pre-created CrudRepository files"
    This reverts commit fe6f05d98ab8d62c7e2376caef6594e17536f812.

---
+++ GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/dao/ItemCategoryRepository.java
@@ -0,0 +1,9 @@
+package mcgill.ecse321.grocerystore.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import mcgill.ecse321.grocerystore.model.ItemCategory;
+
+public interface ItemCategoryRepository extends CrudRepository<ItemCategory, String> {
+
+
+}
---
+++ GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/dao/OpeningHoursRepository.java
@@ -0,0 +1,9 @@
+package mcgill.ecse321.grocerystore.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import mcgill.ecse321.grocerystore.model.OpeningHours;
+
+public interface OpeningHoursRepository extends CrudRepository<OpeningHours, String> {
+
+
+}
