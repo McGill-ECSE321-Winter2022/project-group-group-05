2d4df9587cdeefe5cb6abb44e347c57c499ef3d8
Yida Pan <yida.pan@mail.mcgill.ca>
2022-02-16 13:22:23 -0500

    initial implementation of item

--- GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/model/Item.java
+++ GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/model/Item.java
@@ -1,12 +1,71 @@
 package mcgill.ecse321.grocerystore.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class Item {
 
-  @Id
-  private String name;
+	@Id
+    @GeneratedValue
+	private String name;
+	
+	private double price;
+	private int inventory;
+	private boolean canDeliver;
+	private boolean canPickUp;
+	private boolean isAvailable;
 
+	// This class has no association
+	public Item() {
+		this.price = 0.0;
+		this.name = null;
+		this.inventory = 0;
+		this.canDeliver = false;
+		this.canPickUp = false;
+		this.isAvailable = false;
+
+	}
+
+	public String getname() {
+		return this.name;
+	}
+
+	public double getprice() {
+		return this.price;
+	}
+
+	public int getinventory() {
+		return this.inventory;
+
+	}
+	public boolean getcanDeliver() {
+		return this.canDeliver;
+	}
+	public boolean getcanPickUp() {
+		return this.canPickUp;
+	}
+	public boolean getisAvailable() {
+		return this.isAvailable;
+	}
+
+	public void setname(String name) {
+		this.name = name;
+	}
+	public void setprice(double price) {
+		this.price=price;
+	}
+	public void setinventory(int inventory) {
+		this.inventory=inventory;
+	}
+	public void setcanDeliver(boolean canDeliver ) {
+		this.canDeliver=canDeliver;
+	}
+	public void setcanPickUp(boolean canPickUp) {
+		this.canPickUp=canPickUp;
+	}
+	public void setisAvailable(boolean isAvailable) {
+		this.isAvailable=isAvailable;
+	}
 }
