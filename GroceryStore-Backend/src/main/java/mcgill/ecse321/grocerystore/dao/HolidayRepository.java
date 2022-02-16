742d0c43b48f19f9328cebe44289278de86c9668
oliverqueen1009 <70770995+oliverqueen1009@users.noreply.github.com>
2022-02-16 17:08:37 -0500

    Initial Update on HolidayRepository.java

--- GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/dao/HolidayRepository.java
+++ GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/dao/HolidayRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import mcgill.ecse321.grocerystore.model.Holiday;
 
 public interface HolidayRepository extends CrudRepository<Holiday, String> {
-
+  
+  Holiday findByName(String name);
 
 }
