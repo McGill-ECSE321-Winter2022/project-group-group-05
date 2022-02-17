d3fe9c81cbc0bfe7ced73eb760995826b4c77f1e
Harrison Wang <harrisonwangphs@gmail.com>
2022-02-17 15:42:37 -0500

    fixed improper imports

--- GroceryStore-Backend/src/test/java/mcgill/ecse321/grocerystore/dao/TestEmployeePersistence.java
+++ GroceryStore-Backend/src/test/java/mcgill/ecse321/grocerystore/dao/TestEmployeePersistence.java
@@ -1,7 +1,7 @@
 package mcgill.ecse321.grocerystore.dao;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
