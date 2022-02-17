3979a025ed278c496c7aa7469ddcf86cacdc0525
Harrison Wang <harrisonwangphs@gmail.com>
2022-02-17 14:48:04 -0500

    Completed Persistence Tests for Employee, EmployeeSchedule, and Shift

---
+++ GroceryStore-Backend/src/test/java/mcgill/ecse321/grocerystore/dao/TestEmployeeSchedulePersistence.java
@@ -0,0 +1,122 @@
+package mcgill.ecse321.grocerystore.dao;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import mcgill.ecse321.grocerystore.model.EmployeeSchedule;
+import mcgill.ecse321.grocerystore.model.Shift;
+
+/**
+ * Persistence Tests for EmployeeSchedule Class.
+ * 
+ * @author Harrison Wang
+ */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class TestEmployeeSchedulePersistence {
+
+  @Autowired
+  private EmployeeScheduleRepository employeeScheduleRepository;
+  @Autowired
+  private ShiftRepository shiftRepository;
+
+  @BeforeEach
+  public void clearDatabase() {
+    employeeScheduleRepository.deleteAll();
+    shiftRepository.deleteAll();
+  }
+
+  // Test EmployeeSchedule Object
+  @Test
+  public void testPersistAndLoadEmployeeSchedule() {
+    // Create Test Stub Instance of Shift.
+    String shiftStubName = "testShiftStub";
+    Shift shiftStub = new Shift();
+    shiftStub.setName(shiftStubName);
+    shiftRepository.save(shiftStub);
+
+    // Create Instance of EmployeeSchedule and save it to database.
+    EmployeeSchedule testSchedule = new EmployeeSchedule();
+    testSchedule.setShift(shiftStub);
+    employeeScheduleRepository.save(testSchedule);
+    long testId = testSchedule.getId();
+
+    // Delete Instance of EmployeeSchedule.
+    testSchedule = null;
+
+    // Fetch previously saved EmployeeSchedule.
+    testSchedule = employeeScheduleRepository.findById(testId);
+
+    // Check validity of attributes.
+    assertNotNull(testSchedule);
+    assertEquals(testId, testSchedule.getId());
+  }
+
+  // Test EmployeeSchedule Attribute
+  @Test
+  public void testAttributeEmployee() {
+    // Create Test Stub Instance of Shift.
+    String shiftStubName = "testShiftStub";
+    Shift shiftStub = new Shift();
+    shiftStub.setName(shiftStubName);
+    shiftRepository.save(shiftStub);
+
+    // Create Instance of EmployeeSchedule and save it to database.
+    EmployeeSchedule testSchedule = new EmployeeSchedule();
+    testSchedule.setShift(shiftStub);
+    employeeScheduleRepository.save(testSchedule);
+    long testId = testSchedule.getId();
+
+    // Change date.
+    String newDate = "2000-03-15";
+    testSchedule.setDate(newDate);
+    employeeScheduleRepository.save(testSchedule);
+
+    // Reload testSchedule from database.
+    testSchedule = null;
+    testSchedule = employeeScheduleRepository.findById(testId);
+
+    // Check that attribute was changed.
+    assertEquals(newDate, testSchedule.getDate().toString());
+  }
+
+  // Test EmployeeSchedule Reference
+  @Test
+  public void testReferenceEmployee() {
+    // Create Test Stub Instances of Shift.
+    String shiftStubName = "testShiftStub";
+    Shift shiftStub = new Shift();
+    shiftStub.setName(shiftStubName);
+    shiftRepository.save(shiftStub);
+
+    String newShiftStubName = "newTestShiftStub";
+    Shift newShiftStub = new Shift();
+    newShiftStub.setName(newShiftStubName);
+    shiftRepository.save(newShiftStub);
+
+    // Create Instance of EmployeeSchedule and save it to database.
+    EmployeeSchedule testSchedule = new EmployeeSchedule();
+    testSchedule.setShift(shiftStub);
+    employeeScheduleRepository.save(testSchedule);
+    long testId = testSchedule.getId();
+
+    // Change Shift reference.
+    testSchedule.setShift(newShiftStub);
+    employeeScheduleRepository.save(testSchedule);
+
+    // Reload testSchedule.
+    testSchedule = null;
+    testSchedule = employeeScheduleRepository.findById(testId);
+
+    // Verify that reference to shiftStub was preserved
+    assertNotNull(testSchedule.getShift());
+    assertEquals(newShiftStubName, testSchedule.getShift().getName());
+
+  }
+
+}
---
+++ GroceryStore-Backend/src/test/java/mcgill/ecse321/grocerystore/dao/TestShiftPersistence.java
@@ -0,0 +1,74 @@
+package mcgill.ecse321.grocerystore.dao;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import mcgill.ecse321.grocerystore.model.Shift;
+
+/**
+ * Persistence Tests for Shift Class.
+ * 
+ * @author Harrison Wang
+ */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class TestShiftPersistence {
+
+  @Autowired
+  private ShiftRepository shiftRepository;
+
+  @BeforeEach
+  public void clearDatabase() {
+    shiftRepository.deleteAll();
+  }
+
+  // Test Shift Object
+  @Test
+  public void testPersistAndLoadShift() {
+    // Create an instance of Shift and save it to database.
+    String name = "TestShift";
+    Shift testShift = new Shift();
+    testShift.setName(name);
+    shiftRepository.save(testShift);
+
+    // Delete instance of Shift.
+    testShift = null;
+
+    // Fetch previously saved Employee.
+    testShift = shiftRepository.findByName(name);
+
+    // Check validity of attributes.
+    assertNotNull(testShift);
+    assertEquals(name, testShift.getName());
+  }
+
+  // Test Shift Attribute
+  @Test
+  public void testAttributeShift() {
+    // Create an instance of Shift and save it to database.
+    String name = "TestShift";
+    Shift testShift = new Shift();
+    testShift.setName(name);
+    shiftRepository.save(testShift);
+
+    // Change startTime.
+    String newStartTime = "09:00:00";
+    testShift.setStartTime(newStartTime);
+    shiftRepository.save(testShift);
+
+    // Reload testShift from database.
+    testShift = null;
+    testShift = shiftRepository.findByName(name);
+
+    // Check that attribute was changed.
+    assertEquals(newStartTime, testShift.getStartTime().toString());
+  }
+
+  // Shift does not contain a reference to any other classes.
+
+}
