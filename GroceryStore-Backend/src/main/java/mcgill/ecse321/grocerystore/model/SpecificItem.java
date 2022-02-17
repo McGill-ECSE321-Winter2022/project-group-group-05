176b5dcb35ab98198f592d1ff110c7c14168f2d6
Jim <71361453+jimcv@users.noreply.github.com>
2022-02-16 22:53:53 -0500

    Partial implementation of SpecificItem JPA & CRUD

--- GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/model/SpecificItem.java
+++ GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/model/SpecificItem.java
@@ -1,8 +1,10 @@
 package mcgill.ecse321.grocerystore.model;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class SpecificItem {
@@ -11,4 +13,51 @@ public class SpecificItem {
   @GeneratedValue
   private long id;
 
+  // TODO: set (optional = false) once Item class is implemented
+  @ManyToOne(optional = true, fetch = FetchType.EAGER)
+  private Item item;
+  
+  private int purchaseQuantity;
+  private double purchasePrice;
+  
+  /**
+   * <b>IMPORTANT: Item must be set before saving to database</b>
+   * <br>Providing default constructor for Spring
+   */
+  public SpecificItem() {
+  }
+  
+  /**
+   * <b>IMPORTANT: id is set automatically only after saving to database</b>
+   * <br>Do not use this before saving to database
+   * @return auto generated id
+   */
+  public long getId() {
+    return this.id;
+  }
+  
+  public Item getItem() {
+    return this.item;
+  }
+  
+  public int getPurchaseQuantity() {
+    return this.purchaseQuantity;
+  }
+  
+  public double getPurchasePrice() {
+    return this.purchasePrice;
+  }
+  
+  public void setItem(Item anItem) {
+    this.item = anItem;
+  }
+  
+  public void setPurchaseQuantity(int qty) {
+    this.purchaseQuantity = qty;
+  }
+  
+  public void setPurchasePrice(int price) {
+    this.purchasePrice = price;
+  }
+  
 }
