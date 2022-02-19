1c690b30d5007fca5d645d6b10a36196eb9de9d9
Jim <71361453+jimcv@users.noreply.github.com>
2022-02-19 01:11:55 -0500

    Override hashCode() and equals() in SpecificItem

--- GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/model/SpecificItem.java
+++ GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/model/SpecificItem.java
@@ -16,48 +16,80 @@ public class SpecificItem {
   // TODO: set (optional = false) once Item class is implemented
   @ManyToOne(optional = true, fetch = FetchType.EAGER)
   private Item item;
-  
+
   private int purchaseQuantity;
   private double purchasePrice;
-  
+
   /**
-   * <b>IMPORTANT: Item must be set before saving to database</b>
-   * <br>Providing default constructor for Spring
+   * <b>IMPORTANT: Item must be set before saving to database</b> <br>
+   * Providing default constructor for Spring
    */
-  public SpecificItem() {
-  }
-  
+  public SpecificItem() {}
+
   /**
-   * <b>IMPORTANT: id is set automatically only after saving to database</b>
-   * <br>Do not use this before saving to database
+   * <b>IMPORTANT: id is set automatically only after saving to database</b> <br>
+   * Do not use this before saving to database
+   * 
    * @return auto generated id
    */
   public long getId() {
     return this.id;
   }
-  
+
   public Item getItem() {
     return this.item;
   }
-  
+
   public int getPurchaseQuantity() {
     return this.purchaseQuantity;
   }
-  
+
   public double getPurchasePrice() {
     return this.purchasePrice;
   }
-  
+
   public void setItem(Item anItem) {
     this.item = anItem;
   }
-  
+
   public void setPurchaseQuantity(int qty) {
     this.purchaseQuantity = qty;
   }
-  
+
   public void setPurchasePrice(int price) {
     this.purchasePrice = price;
   }
-  
+
+  /**
+   * At creation, SpecificItems will have default Object.hashCode(). <br>
+   * Once saved to the database, their hashCode() will be their id.
+   */
+  @Override
+  public int hashCode() {
+    if (this.getId() == 0)
+      return super.hashCode();
+    return (int) this.getId();
+  }
+
+
+  /**
+   * Two SpecificItems are not equal if they both have id of 0 because they haven't been saved to
+   * the database yet, unless they're equal by == as defined in Object. <br>
+   * Otherwise, they're equal if they have the same id.
+   */
+  @Override
+  public boolean equals(Object o) {
+    if (o == null)
+      return false;
+    if (o == this)
+      return true;
+    if (o.getClass() == this.getClass()) {
+      SpecificItem obj = (SpecificItem) o;
+      if (obj.getId() == 0)
+        return false;
+      return obj.getId() == this.getId();
+    }
+    return false;
+  }
+
 }
