2c47c40a384631434e660f9cfca4b2d1bbc6008c
Jim <71361453+jimcv@users.noreply.github.com>
2022-02-21 15:28:36 -0500

    Autoremove unused imports

--- GroceryStore-Backend/src/test/java/mcgill/ecse321/grocerystore/dao/TestOpeningHoursPersistence.java
+++ GroceryStore-Backend/src/test/java/mcgill/ecse321/grocerystore/dao/TestOpeningHoursPersistence.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
