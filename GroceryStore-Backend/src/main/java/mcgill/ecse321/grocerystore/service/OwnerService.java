6115f8c86f7c63c61ed0ac2e93eff829b9c22394
oliverqueen1009 <sibo.huang@mail.mcgill.ca>
2022-03-07 01:14:09 -0500

    Create Service Class for Holiday, OpeningHours, Owner & Initial Implementation of OwnerService

---
+++ GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/service/OwnerService.java
@@ -0,0 +1,63 @@
+package mcgill.ecse321.grocerystore.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import mcgill.ecse321.grocerystore.dao.CustomerRepository;
+import mcgill.ecse321.grocerystore.dao.EmployeeRepository;
+import mcgill.ecse321.grocerystore.dao.OwnerRepository;
+import mcgill.ecse321.grocerystore.model.Owner;
+
+@Service
+public class OwnerService {
+  @Autowired
+  OwnerRepository ownerRepository;
+  @Autowired
+  CustomerRepository customerRepository;
+  @Autowired
+  EmployeeRepository employeeRepository;
+
+  @Transactional
+  public Owner createOwner(String username, String password, String email) {
+    if (username == null || username.trim().length() == 0) {
+      throw new IllegalArgumentException("Username cannot be empty!");
+    }
+    if (ownerRepository.findByUsername(username) != null) {
+      throw new IllegalArgumentException("Username is already taken!");
+    }
+    if (customerRepository.findByUsername(username) != null) {
+      throw new IllegalArgumentException("Username is already taken!");
+    }
+    if (employeeRepository.findByUsername(username) != null) {
+      throw new IllegalArgumentException("Username is already taken!");
+    }
+    if (password == null || password.trim().length() == 0) {
+      throw new IllegalArgumentException("Password cannot be emtpy!");
+    }
+    if (email == null || email.trim().length() == 0) {
+      throw new IllegalArgumentException("Email cannot be empty!");
+    }
+    if (email.contains(" ") || !email.contains(".") || email.indexOf("@") < 1
+        || email.indexOf(".") <= email.indexOf("@") + 1
+        || email.lastIndexOf(".") >= email.length() - 1) {
+      throw new IllegalArgumentException("Email is invalid!");
+    }
+
+    Owner owner = new Owner();
+    owner.setUsername(username);
+    owner.setPassword(password);
+    owner.setEmail(email);
+    ownerRepository.save(owner);
+    return owner;
+  }
+
+  @Transactional
+  public Owner getOwner(String username) {
+    if (username == null || username.trim().length() == 0) {
+      throw new IllegalArgumentException("Username cannot be empty!");
+    }
+    Owner owner = ownerRepository.findByUsername(username);
+    return owner;
+  }
+
+}
