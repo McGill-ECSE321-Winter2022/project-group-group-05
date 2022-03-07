e97eb6ba688838afd16b1eff45e4a733ab9f2a37
jimcv <71361453+jimcv@users.noreply.github.com>
2022-03-06 22:16:07 -0500

    Add 'Cancelled' state to PurchaseState

--- GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/model/Purchase.java
+++ GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/model/Purchase.java
@@ -23,7 +23,7 @@ import javax.persistence.OneToMany;
 public class Purchase {
 
   public enum PurchaseState {
-    Cart, Paid, Prepared, Completed
+    Cart, Paid, Prepared, Completed, Cancelled
   };
 
   @Id
