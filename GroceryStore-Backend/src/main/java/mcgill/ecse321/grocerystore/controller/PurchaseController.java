24ad0f49ab2bd009ce62ae36ff75dea0a5090195
jimcv <71361453+jimcv@users.noreply.github.com>
2022-03-07 23:28:16 -0500

    Partial PurchaseController

--- GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/controller/PurchaseController.java
+++ GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/controller/PurchaseController.java
@@ -6,10 +6,14 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import mcgill.ecse321.grocerystore.dao.CustomerRepository;
 import mcgill.ecse321.grocerystore.dto.ItemDto;
 import mcgill.ecse321.grocerystore.dto.PurchaseDto;
 import mcgill.ecse321.grocerystore.dto.SpecificItemDto;
+import mcgill.ecse321.grocerystore.model.Customer;
 import mcgill.ecse321.grocerystore.model.Item;
 import mcgill.ecse321.grocerystore.model.Purchase;
 import mcgill.ecse321.grocerystore.model.SpecificItem;
@@ -22,14 +26,49 @@ public class PurchaseController {
   @Autowired
   private PurchaseService service;
 
+  // TODO: remove this after CustomerController is done
+  @Autowired
+  private CustomerRepository customerRepo;
+
+  @GetMapping(value = {"/purchase/all", "/purchase/all/"})
+  public List<PurchaseDto> getAllPurchases() {
+    return service.getAll().stream().map(p -> this.toDto(p)).collect(Collectors.toList());
+  }
+
   @GetMapping(value = {"/purchase/allDesc", "/purchase/allDesc/"})
   public List<PurchaseDto> getAllPurchasesDesc() {
     return service.getAllDesc().stream().map(p -> this.toDto(p)).collect(Collectors.toList());
   }
 
-  @GetMapping(value = {"/purchase/all", "/purchase/all/"})
-  public List<PurchaseDto> getAllPurchases() {
-    return service.getAll().stream().map(p -> this.toDto(p)).collect(Collectors.toList());
+  @PostMapping(value = {"/purchase/getCart", "/purchase/getCart/"})
+  public PurchaseDto getCart(@RequestParam String username) throws IllegalArgumentException {
+    return this.toDto(service.getCart(username));
+  }
+
+  @PostMapping(value = {"/purchase/pos/newCart", "/purchase/pos/newCart/"})
+  public PurchaseDto posGetNewCart() throws IllegalArgumentException {
+    return this.toDto(service.posGetNewCart());
+  }
+
+  @PostMapping(value = {"/purchase/pos/purge", "/purchase/pos/purge/"})
+  public void posPurgeCarts() throws IllegalArgumentException {
+    service.posPurgeCarts();
+  }
+
+  // TODO: delete this after CustomerController is done
+  @PostMapping(value = {"/purchase/mockCustomer", "/purchase/mockCustomer/"})
+  public void createMockCustomer(@RequestParam String username) {
+    Customer c = new Customer();
+    c.setUsername(username);
+    customerRepo.save(c);
+  }
+
+  private ItemDto toDto(Item i) throws IllegalArgumentException {
+    if (i == null) {
+      throw new IllegalArgumentException("Invalid item.");
+    }
+    return new ItemDto(i.getName(), i.getPrice(), i.getInventory(), i.getCanDeliver(),
+        i.getCanPickUp(), i.getIsDiscontinued());
   }
 
   private PurchaseDto toDto(Purchase p) throws IllegalArgumentException {
@@ -50,14 +89,6 @@ public class PurchaseController {
     }
     return new SpecificItemDto(s.getId(), this.toDto(s.getItem()), s.getPurchaseQuantity(),
         s.getPurchasePrice());
-  }
-
-  private ItemDto toDto(Item i) throws IllegalArgumentException {
-    if (i == null) {
-      throw new IllegalArgumentException("Invalid item.");
-    }
-    return new ItemDto(i.getName(), i.getPrice(), i.getInventory(), i.getCanDeliver(),
-        i.getCanPickUp(), i.getIsDiscontinued());
   }
 
 }
