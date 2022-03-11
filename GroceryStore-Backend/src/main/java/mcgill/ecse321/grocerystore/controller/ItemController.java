07d08bfd7f29d8a98bcecde3d8f4c10f9d414da8
jimcv <71361453+jimcv@users.noreply.github.com>
2022-03-11 00:02:55 -0500

    Add delete() to ItemController

--- GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/controller/ItemController.java
+++ GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/controller/ItemController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,11 @@ public class ItemController {
       @RequestParam int inventory, @RequestParam boolean canDeliver,
       @RequestParam boolean canPickUp) throws IllegalArgumentException {
     return convertToDto(service.createItem(name, price, inventory, canDeliver, canPickUp));
+  }
+
+  @DeleteMapping(value = {"/item/{name}", "/item/{name}/"})
+  public void delete(@PathVariable("name") String name) {
+    service.delete(name);
   }
 
   @GetMapping(value = {"/item/{name}", "/item/{name}/"})
