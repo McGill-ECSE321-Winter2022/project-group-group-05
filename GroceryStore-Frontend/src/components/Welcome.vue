7423b41531b10dd7b4ef95787fdda0ee6f55175a
jimcv <71361453+jimcv@users.noreply.github.com>
2022-03-23 02:44:10 -0400

    Set up Welcome.vue

---
+++ GroceryStore-Frontend/src/components/Welcome.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Welcome",
+  data() {
+    return {
+      msg: "Welcome to the Grocery Store",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
