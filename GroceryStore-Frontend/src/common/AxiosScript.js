78db63eb92d46aaf916b0aba8c9b6fff67ae6bc1
jimcv <71361453+jimcv@users.noreply.github.com>
2022-03-23 02:23:03 -0400

    Add axios script & state script

---
+++ GroceryStore-Frontend/src/common/AxiosScript.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const CONFIG = require("../../config");
+
+const BACKEND_URL = function () {
+  switch (process.env.NODE_ENV) {
+    case "development":
+      // use https if developing using hosted backend, else use http
+      if (CONFIG.dev.backendHost === "grocery-backend-g05-mcgill.herokuapp.com") {
+        return (
+          "https://" + CONFIG.dev.backendHost + ":" + CONFIG.dev.backendPort
+        );
+      } else {
+        return (
+          "http://" + CONFIG.dev.backendHost + ":" + CONFIG.dev.backendPort
+        );
+      }
+    case "production":
+      return (
+        "https://" + CONFIG.build.backendHost + ":" + CONFIG.build.backendPort
+      );
+  }
+};
+
+const FRONTEND_URL = function () {
+  switch (process.env.NODE_ENV) {
+    case "development":
+      return "http://" + CONFIG.dev.host + ":" + CONFIG.dev.port;
+    case "production":
+      return "https://" + CONFIG.build.host + ":" + CONFIG.build.port;
+  }
+};
+
+export const AXIOS = axios.create({
+  baseURL: BACKEND_URL(),
+  headers: { "Access-Control-Allow-Origin": FRONTEND_URL() },
+});
---
+++ GroceryStore-Frontend/src/common/StateScript.js
@@ -0,0 +1,33 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import persistedState from "vuex-persistedstate";
+
+Vue.use(Vuex);
+
+// sessionStorage clears on new tab, localStorage never expires
+
+export const LOGIN_STATE = new Vuex.Store({
+  plugins: [
+    persistedState({
+      key: "vuex-login-state",
+      storage: window.localStorage,
+    }),
+  ],
+  state: {
+    isLoggedIn: false,
+    userType: "",
+    username: "",
+  },
+  mutations: {
+    login(state, { userType, username }) {
+      state.isLoggedIn = true;
+      state.userType = userType;
+      state.username = username;
+    },
+    logout(state) {
+      state.isLoggedIn = false;
+      state.userType = "";
+      state.username = "";
+    },
+  },
+});
