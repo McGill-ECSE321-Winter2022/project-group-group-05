0e9bf630b0ae15bac4a6d83754c0a614f85c16a4
PeiniCheng <peini.cheng@mail.mcgill.ca>
2022-03-24 17:29:08 -0400

    fix typo+remove uneccessary lines+view history

--- GroceryStore-Frontend/src/components/ManageProfile.vue
+++ GroceryStore-Frontend/src/components/ManageProfile.vue
@@ -1,95 +1,103 @@
 <!--Visibility: customer-->
 <!--Modify customer's own account infos-->
 <template>
-  <div id="manageprofile">
-    <h1>Manage Your Profiles</h1>
-    <br />
-    <table>
-      <tr>
-        <td>Username:</td>
-        <td>{{ username }}</td>
-      </tr>
+  <div>
+    <div id="manageprofile">
+      <h1>Manage Your Profile</h1>
+      <br />
+      <table>
+        <tr>
+          <td>Username:</td>
+          <td>{{ username }}</td>
+        </tr>
+        <br />
+        <tr>
+          <td>Email:</td>
+          <td>
+            <input type="text" v-model="customer.email" placeholder="Email" />
+          </td>
+          <td>
+            <b-button
+              variant="outline-primary"
+              v-bind:disabled="!customer.email"
+              @click="updateCustomer(customer.email, null, null, null)"
+            >
+              Save
+            </b-button>
+          </td>
+        </tr>
+        <br />
+        <tr>
+          <td>Password:</td>
+          <td>
+            <input
+              type="text"
+              v-model="customer.password"
+              placeholder="Password"
+            />
+          </td>
+          <td>
+            <b-button
+              variant="outline-primary"
+              v-bind:disabled="!customer.password"
+              @click="updateCustomer(null, customer.password, null, null)"
+            >
+              Save
+            </b-button>
+          </td>
+        </tr>
+        <br />
+
+        <tr>
+          <td>Address:</td>
+          <td>
+            <input
+              type="text"
+              v-model="customer.address"
+              placeholder="Address"
+            />
+          </td>
+          <td>
+            <b-button
+              variant="outline-primary"
+              v-bind:disabled="!customer.address"
+              @click="updateCustomer(null, null, customer.address, null)"
+            >
+              Save
+            </b-button>
+          </td>
+        </tr>
+      </table>
       <br />
       <tr>
-        <td>Email:</td>
         <td>
-          <input type="text" v-model="customer.email" placeholder="Email" />
-        </td>
-        <td>
-          <b-button
-            variant="outline-primary"
-            v-bind:disabled="!customer.email"
-            @click="updateCustomer(customer.email, null, null, null)"
-          >
-            Save
-          </b-button>
+          <h5>Is this address local?</h5>
+          <input
+            type="radio"
+            id="True"
+            value="true"
+            name="isLocal"
+            v-model="customer.local"
+            v-on:click="updateCustomer(null, null, null, true)"
+          />
+          <label for="True">yes</label>
+          <input
+            type="radio"
+            id="False"
+            value="false"
+            name="isLocal"
+            v-model="customer.local"
+            v-on:click="updateCustomer(null, null, null, false)"
+          />
+          <label for="False">no</label>
         </td>
       </tr>
-      <span v-if="errorCustomer" style="color: red"
+    </div>
+    <div>
+      <span id="error" v-if="errorCustomer" style="color: red"
         >Error: {{ errorCustomer }}
       </span>
-      <br />
-      <tr>
-        <td>Password:</td>
-        <td>
-          <input
-            type="text"
-            v-model="customer.password"
-            placeholder="Password"
-          />
-        </td>
-        <td>
-          <b-button
-            variant="outline-primary"
-            v-bind:disabled="!customer.password"
-            @click="updateCustomer(null, customer.password, null, null)"
-          >
-            Save
-          </b-button>
-        </td>
-      </tr>
-      <br />
-
-      <tr>
-        <td>Address:</td>
-        <td>
-          <input type="text" v-model="customer.address" placeholder="Address" />
-        </td>
-        <td>
-          <b-button
-            variant="outline-primary"
-            v-bind:disabled="!customer.address"
-            @click="updateCustomer(null, null, customer.address, null)"
-          >
-            Save
-          </b-button>
-        </td>
-      </tr>
-    </table>
-    <br />
-    <tr>
-      <td>
-        <h4>Is this address local?</h4>
-        <input
-          type="radio"
-          id="True"
-          value="true"
-          name="isLocal"
-          v-model="customer.local"
-          v-on:click="updateCustomer(null, null, null, true)"
-        />
-        <label for="True">yes</label>
-        <input
-          type="radio"
-          id="False"
-          value="false"
-          name="isLocal"
-          v-model="customer.local"
-          v-on:click="updateCustomer(null, null, null, false)"
-        />
-        <label for="False">no</label>
-      </td>
-    </tr>
+    </div>
   </div>
 </template>
 
@@ -103,7 +111,6 @@ export default {
     return {
       customer: AXIOS.get("/customer/".concat(username))
         .then(response => {
-          //JSON responses are automatically parsed
           this.customer = response.data;
         })
         .catch(e => {
@@ -116,7 +123,6 @@ export default {
       response: [],
     };
   },
-
   methods: {
     updateCustomer: function (email, password, address, isLocal) {
       AXIOS.patch(
@@ -132,18 +138,7 @@ export default {
         }
       )
         .then(response => {
-          // JSON responses are automatically parsed.
           this.errorCustomer = "";
-          AXIOS.get("/customer/".concat(username))
-            .then(response => {
-              //JSON responses are automatically parsed
-              this.customer = response.data;
-            })
-            .catch(e => {
-              var errorMsg = e.response.data.message;
-              console.log(errorMsg);
-              this.errorCustomer = errorMsg;
-            });
         })
         .catch(e => {
           var errorMsg = e.response.data.message;
@@ -156,10 +151,6 @@ export default {
 </script>
 
 <style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
 div {
   line-height: 40px;
 }
@@ -168,12 +159,17 @@ input[type="text"] {
   border: none;
   border-bottom: 1px solid #727272;
 }
+#error {
+  position: absolute;
+  top: 210px;
+  right: 500px;
+}
 #manageprofile {
   text-align: center;
   position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  height: 700px;
+  height: 750px;
 }
 </style>
