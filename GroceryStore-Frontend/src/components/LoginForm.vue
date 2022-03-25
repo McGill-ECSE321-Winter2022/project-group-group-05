06412b24111fc651e36a368a81c2f20ef362dee1
jimcv <71361453+jimcv@users.noreply.github.com>
2022-03-24 20:14:04 -0400

    Initial implementation of LoginForm

--- GroceryStore-Frontend/src/components/LoginForm.vue
+++ GroceryStore-Frontend/src/components/LoginForm.vue
@@ -1,11 +1,74 @@
 <!--Visibility: notLoggedIn-->
 <!--Unified login form for everyone-->
-<template></template>
+<template>
+  <div class="login-form" v-if="!LOGIN_STATE.state.isLoggedIn">
+    <b-overlay :show="isLoading" rounded="sm">
+      <div :aria-hidden="isLoading ? 'true' : null" class="form-area">
+        <p class="h2 text-center mb-5">Sign In to Your Account</p>
+        <b-form @submit="onsubmit">
+          <b-form-group
+            id="username-input-group"
+            label="Your username"
+            label-for="username-input"
+            label-size="lg"
+          >
+            <b-form-input
+              id="username-input"
+              v-model="form.username"
+              placeholder="Enter username"
+              required
+            ></b-form-input>
+          </b-form-group>
+          <b-form-group
+            id="password-input-group"
+            label="Your password"
+            label-for="password-input"
+            label-size="lg"
+          >
+            <b-form-input
+              type="password"
+              id="password-input"
+              v-model="form.password"
+              placeholder="Enter password"
+              required
+            ></b-form-input>
+          </b-form-group>
+          <div class="text-center" v-show="formError">
+            <p style="color: red">{{ formError }}</p>
+          </div>
+          <div class="text-center mt-5">
+            <b-button
+              type="submit"
+              variant="primary"
+              v-bind:disabled="isLoading"
+              >Sign in
+            </b-button>
+          </div>
+        </b-form>
+      </div>
 
-<script>
-export default {
-  name: "LoginForm",
-};
-</script>
+      <template #overlay>
+        <div class="text-center h2">
+          <b-icon icon="stopwatch" font-scale="3" animation="cylon"></b-icon>
+          <p id="wait-message">Please wait...</p>
+        </div>
+      </template>
+    </b-overlay>
+  </div>
+</template>
 
-<style scoped></style>
+<script src="./LoginFormScript.js"></script>
+
+<style scoped>
+.login-form {
+  border: 1px solid black;
+  width: 600px;
+  margin-top: 100px;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: left;
+}
+.form-area {
+  padding: 100px;
+}
+</style>
---
+++ GroceryStore-Frontend/src/components/LoginFormScript.js
@@ -0,0 +1,72 @@
+import { LOGIN_STATE } from "../common/StateScript";
+import { AXIOS } from "../common/AxiosScript";
+
+export default {
+  name: "LoginForm",
+  data() {
+    return {
+      LOGIN_STATE,
+      form: {
+        username: "",
+        password: "",
+      },
+      formError: "",
+      isLoading: false,
+    };
+  },
+  methods: {
+    onsubmit(event) {
+      // disable the default response on submit buttons
+      event.preventDefault();
+      // find the user in Owner, Employee, then Customer
+      let username = this.form.username;
+      let password = this.form.password;
+      this.formError = "";
+      this.isLoading = true;
+      AXIOS.get("/owner/".concat(username), {})
+        .then(response => {
+          if (password === response.data.password) {
+            let userType = "Owner";
+            LOGIN_STATE.commit("login", { userType, username });
+            this.isLoading = false;
+            this.$router.push("/ManageStaffProfile");
+          } else {
+            this.formError = "Wrong password";
+            this.isLoading = false;
+          }
+        })
+        .catch(e => {
+          AXIOS.get("/employee/".concat(username), {})
+            .then(response => {
+              if (password === response.data.password) {
+                let userType = "Employee";
+                LOGIN_STATE.commit("login", { userType, username });
+                this.isLoading = false;
+                this.$router.push("/ManageStaffProfile");
+              } else {
+                this.formError = "Wrong password";
+                this.isLoading = false;
+              }
+            })
+            .catch(e => {
+              AXIOS.get("/customer/".concat(username), {})
+                .then(response => {
+                  if (password === response.data.password) {
+                    let userType = "Customer";
+                    LOGIN_STATE.commit("login", { userType, username });
+                    this.isLoading = false;
+                    this.$router.push("/");
+                  } else {
+                    this.formError = "Wrong password";
+                    this.isLoading = false;
+                  }
+                })
+                .catch(e => {
+                  this.formError = "User does not exist";
+                  this.isLoading = false;
+                });
+            });
+        });
+    },
+  },
+};
