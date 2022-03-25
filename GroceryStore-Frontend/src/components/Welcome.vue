6e86e19558c092dae5db4e861b8f6cddbf8ed4a9
jimcv <71361453+jimcv@users.noreply.github.com>
2022-03-24 21:01:31 -0400

    Upon creation of Welcome, check if logged in user is still in system

--- GroceryStore-Frontend/src/components/Welcome.vue
+++ GroceryStore-Frontend/src/components/Welcome.vue
@@ -1,43 +1,58 @@
 <!--Landing page of the application-->
 <template>
-  <div>
-    <!--Temporary skeleton to provide functionalities but not looks-->
-    <h1>{{ msg }}</h1>
-    <router-link to="/LoginForm">
-      <b-button variant="success" v-if="!LOGIN_STATE.state.isLoggedIn"
-        >Sign in</b-button
-      >
-    </router-link>
-    <b-button
-      variant="outline-info"
-      v-if="LOGIN_STATE.state.isLoggedIn"
-      @click="logout()"
-    >
-      <b-icon icon="power" aria-hidden="true"></b-icon> Sign out</b-button
-    >
-    <router-link to="/CreateCustomerForm">
-      <b-button variant="primary" v-if="!LOGIN_STATE.state.isLoggedIn"
-        >Create account</b-button
-      >
-    </router-link>
-    <hr />
-    <router-link to="/ManageProfile">
-      <b-button pill variant="info" v-if="isCustomer">My profile</b-button>
-    </router-link>
-    <router-link to="/ManageStaffProfile">
-      <b-button pill variant="info" v-if="isStaff">My staff profile</b-button>
-    </router-link>
-    <router-link to="/ViewHistory">
-      <b-button pill variant="info" v-if="isCustomer"
-        >My purchase history</b-button
-      >
-    </router-link>
-    <router-link to="/ManageCart">
-      <b-button pill variant="info" v-if="isCustomer">My cart</b-button>
-    </router-link>
+  <div id="welcome-outer">
+    <b-overlay :show="isLoading" rounded="sm">
+      <div :aria-hidden="isLoading ? 'true' : null" id="welcome-inner">
+        <!--Temporary skeleton to provide functionalities but not looks-->
+        <h1>{{ msg }}</h1>
+        <router-link to="/LoginForm">
+          <b-button variant="success" v-if="!LOGIN_STATE.state.isLoggedIn"
+          >Sign in</b-button
+          >
+        </router-link>
+        <b-button
+          variant="outline-info"
+          v-if="LOGIN_STATE.state.isLoggedIn"
+          @click="logout()"
+        >
+          <b-icon icon="power" aria-hidden="true"></b-icon> Sign out</b-button
+        >
+        <router-link to="/CreateCustomerForm">
+          <b-button variant="primary" v-if="!LOGIN_STATE.state.isLoggedIn"
+          >Create account</b-button
+          >
+        </router-link>
+        <hr />
+        <router-link to="/ManageProfile">
+          <b-button pill variant="info" v-if="isCustomer">My profile</b-button>
+        </router-link>
+        <router-link to="/ManageStaffProfile">
+          <b-button pill variant="info" v-if="isStaff">My staff profile</b-button>
+        </router-link>
+        <router-link to="/ViewHistory">
+          <b-button pill variant="info" v-if="isCustomer"
+          >My purchase history</b-button
+          >
+        </router-link>
+        <router-link to="/ManageCart">
+          <b-button pill variant="info" v-if="isCustomer">My cart</b-button>
+        </router-link>
+      </div>
+
+      <template #overlay>
+        <div class="text-center h2">
+          <b-icon icon="stopwatch" font-scale="3" animation="cylon"></b-icon>
+          <p id="wait-message">Please wait...</p>
+        </div>
+      </template>
+    </b-overlay>
   </div>
 </template>
 
 <script src="./WelcomeScript.js"></script>
 
-<style scoped></style>
+<style scoped>
+#welcome-inner {
+  padding: 100px;
+}
+</style>
--- GroceryStore-Frontend/src/components/WelcomeScript.js
+++ GroceryStore-Frontend/src/components/WelcomeScript.js
@@ -1,4 +1,5 @@
 import { LOGIN_STATE } from "../common/StateScript";
+import { AXIOS } from "../common/AxiosScript";
 
 export default {
   name: "Welcome",
@@ -10,7 +11,47 @@ export default {
         LOGIN_STATE.state.userType === "Employee" ||
         LOGIN_STATE.state.userType === "Owner",
       isCustomer: LOGIN_STATE.state.userType === "Customer",
+      isLoading: false,
     };
+  },
+  created: function () {
+    // upon creation, verify if stored logged in user is still in the system
+    let userType = LOGIN_STATE.state.userType;
+    let username = LOGIN_STATE.state.username;
+    if (LOGIN_STATE.state.isLoggedIn) {
+      this.isLoading = true;
+      if (userType === "Owner") {
+        AXIOS.get("/owner/".concat(username), {})
+          .then(response => {
+            this.isLoading = false;
+          })
+          .catch(e => {
+            this.isLoading = false;
+            this.logout();
+          });
+      } else if (userType === "Employee") {
+        AXIOS.get("/employee/".concat(username), {})
+          .then(response => {
+            this.isLoading = false;
+          })
+          .catch(e => {
+            this.isLoading = false;
+            this.logout();
+          });
+      } else if (userType === "Customer") {
+        AXIOS.get("/customer/".concat(username), {})
+          .then(response => {
+            this.isLoading = false;
+          })
+          .catch(e => {
+            this.isLoading = false;
+            this.logout();
+          });
+      } else {
+        this.isLoading = false;
+        this.logout();
+      }
+    }
   },
   methods: {
     logout: function () {
