a90df991b3ddbd0944f846e79180f4b590845007
Annie Kang <yiwen.kang@mail.mcgill.ca>
2022-03-26 22:03:12 -0400

    manage paid orders vue+js complete

--- GroceryStore-Frontend/src/components/ManagePaidOrders.vue
+++ GroceryStore-Frontend/src/components/ManagePaidOrders.vue
@@ -1,11 +1,130 @@
 <!--Visibility: owner, employee-->
 <!--Show all paid orders and be able to change them to prepared-->
-<template></template>
+<template>
+  <div id="manage-paid-orders">
+    <div class="back-to-previous-style">
+            <a href="javascript:history.back()">Previous Page</a>
+    </div>
 
-<script>
-export default {
-  name: "ManagePaidOrders",
-};
-</script>
+    <h1 class="header_style">Manage Paid Orders</h1><br>
+    
+    <table id="purchaseTable" v-for="purchase in purchases" :key="purchase.id">
+      <div id="header">
+        <td class="tdstyle">{{ purchase.state }} &nbsp; &nbsp; &nbsp;</td>
+        <td class="tdstyle">Date: {{ purchase.dateOfPurchase }} &nbsp;</td>
+        <td class="tdstyle">Order# {{ purchase.id }} &nbsp;</td>
+        <td class="tdstyle">Order type: {{ orderType(purchase) }} &nbsp;</td>
+        <td>
+          <b-button
+            pill variant="info"
+            id="Prepare"
+            v-on:click="prepare(purchase.id)"
+            >Prepare Order</b-button
+          >
+        </td>
+      </div>
+      <table id="itemTable">
+        <tr>
+          <th>item</th>
+          <th>price</th>
+          <th>quantity</th>
+        </tr>
+        <tr
+          v-for="specificItem in purchase.specificItems"
+          :key="specificItem.id"
+        >
+          <td>{{ specificItem.item.name }}</td>
+          <td>${{ specificItem.purchasePrice | formatCurrency }}</td>
+          <td>
+            {{ specificItem.purchaseQuantity }}
+            {{
+              addToTotal(
+                specificItem.purchaseQuantity * specificItem.purchasePrice
+              )
+            }}
+          </td>
+        </tr>
+      </table>
+      <hr />
+      <tr id="totalPrice">
+        Total: ${{
+          totalPrice | formatCurrency
+        }}
+        {{
+          clearSum()
+        }}
+      </tr>
+      <br />
+    </table>
+  </div>
+</template>
 
-<style scoped></style>
+<script src="./ManagePaidOrdersScript.js"></script>
+
+<style scoped>
+/* Styling for Page Title */
+.header_style {
+  padding-top: 15px;
+  padding-left: 100px;
+  border-color: #91c788;
+  border-style: solid;
+  border-width: 0px 0px 6px 0px;
+  text-align: center;
+  margin-bottom: 30px;
+}
+.tdstyle{
+  border: 1px solid #dddddd;
+  text-align: center;
+  padding: 8px;
+}
+#cancel {
+  position: absolute;
+  right: 0px;
+}
+#confirm {
+  position: absolute;
+  right: 0px;
+}
+#header {
+  border-radius: 5px;
+  position: relative;
+  background-color: rgb(231, 234, 240);
+  display: flex;
+  flex-direction: row;
+  height: 40px;
+  text-align: bottom;
+}
+#manage-paid-orders {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 60%;
+  transform: translate(-50%, -50%);
+  height: 750px;
+}
+#purchaseTable {
+  position: relative;
+  width: 100%;
+}
+#itemTable {
+  width: 80%;
+  text-align: left;
+}
+#totalPrice {
+  position: absolute;
+  right: 200px;
+  bottom: 10px;
+}
+.back-to-previous-style {
+  padding: 20px 1px;
+  font-size: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 180px;
+  text-align: center;
+}
+a {
+  color: #2410da;
+}
+</style>
---
+++ GroceryStore-Frontend/src/components/ManagePaidOrdersScript.js
@@ -0,0 +1,50 @@
+import { LOGIN_STATE } from "../common/StateScript";
+import { AXIOS } from "../common/AxiosScript";
+export default {
+  name: "ViewPaidOrder",
+  data() {
+    return {
+      purchases: AXIOS.get("/purchase/allPaid",
+        {},
+        {}
+      )
+        .then(response => {
+          this.errorPurchase = "";
+          this.purchases = response.data;
+        })
+        .catch(e => {
+          var errorMsg = e.response.data.message;
+          console.log(errorMsg);
+          this.errorPurchase = errorMsg;
+        }),
+      totalPrice: 0,
+    };
+  },
+  methods: {
+    orderType(purchase) {
+      if (purchase.delivery) {
+        return "delivery";
+      } else {
+        return "pick up";
+      }
+    },
+    addToTotal(price) {
+      this.totalPrice += price;
+    },
+    clearSum() {
+      this.totalPrice = 0;
+    },
+    prepare: function (id) {
+      AXIOS.post("/purchase/prepare/".concat(id), {}, {})
+        .then(response => {
+          this.errorPurchase = "";
+        })
+        .catch(e => {
+          var errorMsg = e.response.data.message;
+          console.log(errorMsg);
+          this.errorPurchase = errorMsg;
+        });
+      window.location.reload();
+    },
+  },
+};
