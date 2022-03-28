1c1571d95d145921bc835f85631ea282078e4755
oliverqueen1009 <sibo.huang@mail.mcgill.ca>
2022-03-28 19:59:07 -0400

    Done StaffDashboard

--- GroceryStore-Frontend/src/components/StaffDashboard.vue
+++ GroceryStore-Frontend/src/components/StaffDashboard.vue
@@ -5,12 +5,118 @@
 <!--Action Button: Purge All POS Carts (call purgePOSCarts service)-->
 
 <!--Visibility of buttons should match visibility of corresponding component-->
-<template></template>
+<template>
+  <div class="center">
+    <b-overlay :show="isLoading" rounded="sm">
+      <div :aria-hidden="isLoading ? 'true' : null" id="staffdashboard-inner">
+
+        <h1>{{ msg }}</h1>
+
+        <router-link to="/ManageCategories">
+          <b-button block variant="success btn" v-if="isOwner"
+            >Manage Categories</b-button
+          >
+        </router-link>
+
+        <router-link to="/ManageCustomers">
+          <b-button block variant="success btn" v-if="isEmployee || isOwner"
+            >Manage Customers</b-button
+          >
+        </router-link>
+
+        <router-link to="/ManageEmployee">
+          <b-button block variant="success btn" v-if="isOwner"
+            >Manage Employee</b-button
+          >
+        </router-link>
+
+        <router-link to="/ManageItems">
+          <b-button block variant="success btn" v-if="isEmployee || isOwner"
+            >Manage Items</b-button
+          >
+        </router-link>
+
+        <router-link to="/ManageOpeningHours">
+          <b-button block variant="success btn" v-if="isOwner"
+            >Manage Opening Hours</b-button
+          >
+        </router-link>
+
+        <router-link to="/ManagePaidOrders">
+          <b-button block variant="success btn" v-if="isEmployee || isOwner"
+            >Manage Paid Orders</b-button
+          >
+        </router-link>
+
+        <router-link to="/ManageSchedules">
+          <b-button block variant="success btn" v-if="isOwner"
+            >Manage Schedules</b-button
+          >
+        </router-link>
+
+        <router-link to="/ManageStaffProfile">
+          <b-button block variant="success btn" v-if="isEmployee || isOwner"
+            >Manage Staff Profile</b-button
+          >
+        </router-link>
+
+        <router-link to="/ViewCompletedOrders">
+          <b-button block variant="success btn" v-if="isEmployee || isOwner"
+            >View Completed Orders</b-button
+          >
+        </router-link>
+
+        <router-link to="/ViewSchedules">
+          <b-button block variant="success btn" v-if="isEmployee"
+            >View Schedules</b-button
+          >
+        </router-link>
+      </div>
+
+      <template #overlay>
+        <div class="text-center">
+          <b-spinner></b-spinner>
+          <p class="h2">Waiting for database...</p>
+        </div>
+      </template>
+    </b-overlay>
+  </div>
+</template>
 
 <script>
+import { LOGIN_STATE } from "../common/StateScript";
+
 export default {
   name: "StaffDashboard",
+  data() {
+    return {
+      LOGIN_STATE,
+      isEmployee:
+        LOGIN_STATE.state.userType === "Employee",
+      isOwner: 
+        LOGIN_STATE.state.userType === "Owner",
+    };
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.center {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  border: 10px solid #ffffff;
+  padding: 10px;
+  margin: auto;
+  width: 60%;
+}
+#staffdashboard-inner {
+  padding: 80px;
+}
+
+.btn {
+    margin: 15px auto;
+}
+
+</style>
