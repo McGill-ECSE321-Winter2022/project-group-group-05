2206298ef07f985e4d79cc673ce8f6a293e08f08
jimcv <71361453+jimcv@users.noreply.github.com>
2022-03-28 00:55:05 -0400

    Implement filter checkboxes

--- GroceryStore-Frontend/src/components/Welcome.vue
+++ GroceryStore-Frontend/src/components/Welcome.vue
@@ -109,91 +109,145 @@
     <b-overlay :show="isItemLoading" rounded="sm">
       <div :aria-hidden="isItemLoading ? 'true' : null" id="items-inner">
         <h2>Browse our in stock items</h2>
-        <div id="in-stock-items-outer">
-          <b-input
-            type="text"
-            v-model="itemSearchQuery"
-            placeholder="Search item"
-          ></b-input>
-          <b-pagination
-            v-model="currentPage"
-            :total-rows="numRows"
-            :per-page="perPage"
-            aria-controls="in-stock-items-table"
-          ></b-pagination>
-          <b-table
-            id="in-stock-items-table"
-            borderless
-            hover
-            :per-page="perPage"
-            :current-page="currentPage"
-            :items="filteredItemList"
-            @row-clicked="addItemDialog"
-          >
-            <template #cell(name)="data">
-              <b>{{ data.value }}</b>
-            </template>
-            <template #cell(image)="data">
-              <b-img-lazy
-                thumbnail
-                style="max-width: 150px"
-                :src="data.value ? data.value : '/static/no-image.jpg'"
-              ></b-img-lazy>
-            </template>
-            <template #cell(price)="data">
-              <h5>${{ data.value | formatCurrency }}</h5>
-            </template>
-            <template #cell(inventory)="data">
-              {{ data.value }} in stock
-            </template>
-          </b-table>
 
-          <b-modal
-            id="add-item-dialog"
-            title="Add Item to Cart"
-            centered
-            @ok="addItemToCart"
-          >
-            <div id="add-item-dialog-outer">
-              <p class="mb-3">
-                Select quantity of <b>{{ clickedItem["name"] }}</b> to add to
-                cart:
-              </p>
-              <div style="width: 200px; margin: auto; padding-bottom: 10px">
-                <b-form-spinbutton
-                  id="quantity-spin"
-                  v-model="addQuantity"
-                  wrap
-                  min="1"
-                  :max="clickedItem['inventory']"
-                  size="lg"
-                ></b-form-spinbutton>
+        <b-container fluid>
+          <b-row>
+            <b-col md="auto" style="text-align: left">
+              <h5>Filters</h5>
+              <b-checkbox v-model="mustCanDeliver">available for delivery</b-checkbox>
+              <b-checkbox v-model="mustCanPickUp">available for pick-up</b-checkbox>
+              <b-checkbox v-model="mustAvailableOnline">available online</b-checkbox>
+              <b-checkbox v-model="showOutOfStock">show out-of-stock items</b-checkbox>
+            </b-col>
+
+            <b-col>
+              <div id="in-stock-items-outer">
+                <b-input
+                  type="text"
+                  v-model="itemSearchQuery"
+                  placeholder="Search item"
+                ></b-input>
+                <br />
+
+                <b-pagination
+                  v-model="currentPage"
+                  :total-rows="numRows"
+                  :per-page="perPage"
+                  aria-controls="in-stock-items-table"
+                ></b-pagination>
+
+                <b-table
+                  id="in-stock-items-table"
+                  borderless
+                  hover
+                  :per-page="perPage"
+                  :current-page="currentPage"
+                  :items="filteredItemList"
+                  :fields="itemListFields"
+                  @row-clicked="addItemDialog"
+                >
+                  <template #cell(name)="data">
+                    <b>{{ data.value }}</b>
+                  </template>
+                  <template #cell(image)="data">
+                    <b-img-lazy
+                      thumbnail
+                      style="max-width: 150px"
+                      :src="data.value ? data.value : '/static/no-image.jpg'"
+                    ></b-img-lazy>
+                  </template>
+                  <template #cell(price)="data">
+                    <h5>${{ data.value | formatCurrency }}</h5>
+                  </template>
+                  <template #cell(inventory)="data">
+                    {{ data.value }} in stock
+                  </template>
+                  <template #cell(canDeliver)="data">
+                    <div v-if="data.value" v-b-tooltip title="Can be delivered">
+                      <b-icon
+                        icon="truck"
+                        font-scale="1.5"
+                        v-b-tooltip
+                        title="Hello!"
+                        variant="success"
+                      ></b-icon>
+                    </div>
+                  </template>
+                  <template #cell(canPickUp)="data">
+                    <div
+                      v-if="data.value"
+                      v-b-tooltip
+                      title="Available for pick-up"
+                    >
+                      <b-icon
+                        icon="bag-check"
+                        font-scale="1.5"
+                        variant="success"
+                      ></b-icon>
+                    </div>
+                    <div v-else v-b-tooltip title="Not available for pick-up">
+                      <b-icon
+                        icon="bag-x"
+                        font-scale="1.5"
+                        variant="danger"
+                      ></b-icon>
+                    </div>
+                  </template>
+                </b-table>
+
+                <b-modal
+                  id="add-item-dialog"
+                  title="Add Item to Cart"
+                  centered
+                  @ok="addItemToCart"
+                >
+                  <div id="add-item-dialog-outer">
+                    <p class="mb-3">
+                      Select quantity of <b>{{ clickedItem["name"] }}</b> to add
+                      to cart:
+                    </p>
+                    <div
+                      style="width: 200px; margin: auto; padding-bottom: 10px"
+                    >
+                      <b-form-spinbutton
+                        id="quantity-spin"
+                        v-model="addQuantity"
+                        wrap
+                        min="1"
+                        :max="clickedItem['inventory']"
+                        size="lg"
+                      ></b-form-spinbutton>
+                    </div>
+                  </div>
+                </b-modal>
+
+                <b-modal
+                  id="add-item-denied"
+                  title="Add Item to Cart"
+                  centered
+                  ok-only
+                >
+                  <div id="add-item-denied-outer">
+                    <p class="mt-2 mb-2">
+                      Sorry, but you cannot add items to your cart because
+                      you're not logged in as a customer.
+                    </p>
+                  </div>
+                </b-modal>
+
+                <b-modal id="add-item-success" title="Success" centered ok-only>
+                  <p class="mt-2 mb-2" style="color: green">
+                    {{ addItemSuccess }}
+                  </p>
+                </b-modal>
+
+                <b-modal id="add-item-error" title="Error" centered ok-only>
+                  <p class="mt-2 mb-2" style="color: red">{{ addItemError }}</p>
+                </b-modal>
               </div>
-            </div>
-          </b-modal>
-
-          <b-modal
-            id="add-item-denied"
-            title="Add Item to Cart"
-            centered
-            ok-only
-          >
-            <div id="add-item-denied-outer">
-              <p class="mt-2 mb-2">
-                Sorry, but you cannot add items to your cart because you're not
-                logged in as a customer.
-              </p>
-            </div>
-          </b-modal>
-
-          <b-modal id="add-item-success" title="Success" centered ok-only>
-            <p class="mt-2 mb-2" style="color: green">{{ addItemSuccess }}</p>
-          </b-modal>
-
-          <b-modal id="add-item-error" title="Error" centered ok-only>
-            <p class="mt-2 mb-2" style="color: red">{{ addItemError }}</p>
-          </b-modal>
-        </div>
+            </b-col>
+          </b-row>
+        </b-container>
       </div>
 
       <template #overlay>
