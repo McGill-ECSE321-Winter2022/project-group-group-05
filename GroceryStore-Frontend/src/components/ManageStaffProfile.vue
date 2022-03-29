394d9390cb4064b2e13f0cde197b59554501469f
oliverqueen1009 <sibo.huang@mail.mcgill.ca>
2022-03-29 00:53:43 -0400

    Update ManageStaffProfile (need to add nested dashboard)

--- GroceryStore-Frontend/src/components/ManageStaffProfile.vue
+++ GroceryStore-Frontend/src/components/ManageStaffProfile.vue
@@ -63,6 +63,68 @@
         >Error: {{ errorEmployee }}
       </span>
     </div>
+
+    <div id="managestaffprofile" v-if="userType === 'Owner'">
+      <b-overlay
+        id="overlay"
+        :show="isLoading"
+        :variant="variant"
+        :opacity="0.85"
+        rounded="sm"
+      >
+        <h1>Manage Your Profile</h1>
+        <br />
+        <table>
+          <tr>
+            <td>Username:</td>
+            <td>{{ username }}</td>
+          </tr>
+          <br />
+          <tr>
+            <td>Email:</td>
+            <td>
+              <input type="text" v-model="owner.email" placeholder="Email" />
+            </td>
+            <td>
+              <b-button
+                variant="outline-primary"
+                v-bind:disabled="!owner.email"
+                @click="updateOwner(owner.email, null)"
+              >
+                Save
+              </b-button>
+            </td>
+          </tr>
+          <br />
+          <tr>
+            <td>Password:</td>
+            <td>
+              <input
+                type="text"
+                v-model="owner.password"
+                placeholder="Password"
+              />
+            </td>
+            <td>
+              <b-button
+                variant="outline-primary"
+                v-bind:disabled="!owner.password"
+                @click="updateOwner(null, owner.password)"
+              >
+                Save
+              </b-button>
+            </td>
+          </tr>
+        </table>
+
+      </b-overlay>
+    </div>
+    <div>
+      <span id="error" v-if="errorOwner" style="color: red"
+        >Error: {{ errorOwner }}
+      </span>
+    </div>
+
   </div>
 </template>
 
@@ -74,28 +136,45 @@ export default {
   data() {
     return {
       employee: "",
+      owner: "",
       username: LOGIN_STATE.state.username,
       userType: LOGIN_STATE.state.userType,
       isLoading: false,
       errorEmployee: "",
+      errorOwner: "",
       variant: "light",
       response: [],
     };
   },
+
   created: function () {
     this.isLoading = true;
     AXIOS.get("/employee/".concat(LOGIN_STATE.state.username))
       .then(response => {
         this.employee = response.data;
+        this.isLoading = false;
       })
+
+      .catch(e => {
+        AXIOS.get("/owner/".concat(LOGIN_STATE.state.username))
+        .then(response => {
+          this.owner = response.data;
+          this.isLoading = false;
+        })
+      })
+
       .catch(e => {
         var errorMsg = e.response.data.message;
         console.log(errorMsg);
         this.errorEmployee = errorMsg;
       })
-      .finally(() => {
-        this.isLoading = false;
-      });
+
+      .catch(e => {
+        var errorMsg = e.response.data.message;
+        console.log(errorMsg);
+        this.errorOwner = errorMsg;
+      })
+
   },
   methods: {
     updateEmployee: function (email, password) {
@@ -125,6 +204,33 @@ export default {
           this.isLoading = false;
         });
     },
+    updateOwner: function (email, password) {
+      this.isLoading = true;
+      AXIOS.patch(
+        "/owner/".concat(LOGIN_STATE.state.username),
+        {},
+        {
+          params: {
+            email: email,
+            password: password,
+          },
+        }
+      )
+        .then(response => {
+          this.errorOwner = "";
+        })
+        .catch(e => {
+          var errorMsg = e.response.data.message;
+          console.log(errorMsg);
+          this.errorOwner = errorMsg;
+        })
+        .finally(() => {
+          if (this.errorOwner === "") {
+            window.location.reload();
+          }
+          this.isLoading = false;
+        });
+    },
   },
 };
 </script>
@@ -146,7 +252,7 @@ input[type="text"] {
 #managestaffprofile {
   text-align: center;
   position: fixed;
-  top: 50%;
+  top: 70%;
   left: 50%;
   transform: translate(-50%, -50%);
   height: 750px;
