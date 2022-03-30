4f73812e098a0b3e21af8caecd5ac84e99dcea33
jimcv <71361453+jimcv@users.noreply.github.com>
2022-03-30 16:25:18 -0400

    Fix wrong purge button action + style of StaffDashboard

--- GroceryStore-Frontend/src/components/StaffDashboard.vue
+++ GroceryStore-Frontend/src/components/StaffDashboard.vue
@@ -6,80 +6,73 @@
 
 <!--Visibility of buttons should match visibility of corresponding component-->
 <template>
-  <div class="center">
+  <div id="dashboard-outer">
     <b-overlay :show="isLoading" rounded="sm">
-      <div :aria-hidden="isLoading ? 'true' : null" id="staffdashboard-inner">
-
-        <h1>{{ msg }}</h1>
+      <div :aria-hidden="isLoading ? 'true' : null" id="dashboard-inner">
 
         <router-link to="/ManageCategories">
-          <b-button block variant="success btn" v-if="isOwner"
+          <b-button squared block size="lg" variant="success btn" v-if="isOwner"
             >Manage Categories</b-button
           >
         </router-link>
 
         <router-link to="/ManageCustomers">
-          <b-button block variant="success btn" v-if="isEmployee || isOwner"
+          <b-button squared block size="lg" variant="success btn" v-if="isEmployee || isOwner"
             >Manage Customers</b-button
           >
         </router-link>
 
         <router-link to="/ManageEmployee">
-          <b-button block variant="success btn" v-if="isOwner"
+          <b-button squared block size="lg" variant="success btn" v-if="isOwner"
             >Manage Employee</b-button
           >
         </router-link>
 
         <router-link to="/ManageItems">
-          <b-button block variant="success btn" v-if="isEmployee || isOwner"
+          <b-button squared block size="lg" variant="success btn" v-if="isEmployee || isOwner"
             >Manage Items</b-button
           >
         </router-link>
 
         <router-link to="/ManageOpeningHours">
-          <b-button block variant="success btn" v-if="isOwner"
+          <b-button squared block size="lg" variant="success btn" v-if="isOwner"
             >Manage Opening Hours</b-button
           >
         </router-link>
 
         <router-link to="/ManagePaidOrders">
-          <b-button block variant="success btn" v-if="isEmployee || isOwner"
+          <b-button squared block size="lg" variant="success btn" v-if="isEmployee || isOwner"
             >Manage Paid Orders</b-button
           >
         </router-link>
 
         <router-link to="/ManageSchedules">
-          <b-button block variant="success btn" v-if="isOwner"
+          <b-button squared block size="lg" variant="success btn" v-if="isOwner"
             >Manage Schedules</b-button
           >
         </router-link>
 
         <router-link to="/ManageStaffProfile">
-          <b-button block variant="success btn" v-if="isEmployee || isOwner"
+          <b-button squared block size="lg" variant="success btn" v-if="isEmployee || isOwner"
             >Manage Staff Profile</b-button
           >
         </router-link>
 
         <router-link to="/ViewCompletedOrders">
-          <b-button block variant="success btn" v-if="isEmployee || isOwner"
+          <b-button squared block size="lg" variant="success btn" v-if="isEmployee || isOwner"
             >View Completed Orders</b-button
           >
         </router-link>
 
         <router-link to="/ViewSchedules">
-          <b-button block variant="success btn" v-if="isEmployee"
+          <b-button squared block size="lg" variant="success btn" v-if="isEmployee"
             >View Schedules</b-button
           >
         </router-link>
 
-        <router-link to="/PointOfSale">
-          <b-button 
-          block variant="danger btn" 
-          v-if="isOwner"
-          v-on:click="purgePOSCarts()"
-            >Purge All POS Carts</b-button
-          >
-        </router-link>
+        <b-button squared block size="lg" variant="danger" v-if="isOwner" v-on:click="purgePOSCarts()">
+          Purge All POS Carts
+        </b-button>
       </div>
 
       <template #overlay>
@@ -103,38 +96,24 @@ export default {
       LOGIN_STATE,
       isEmployee:
         LOGIN_STATE.state.userType === "Employee",
-      isOwner: 
+      isOwner:
         LOGIN_STATE.state.userType === "Owner",
         isLoading: false,
     };
   },
 
   methods: {
-    purgePOSCarts() {
+    purgePOSCarts: async function() {
       this.isLoading = true;
-      AXIOS.delete("/purchase/pos/delete")
+      await AXIOS.delete("/purchase/pos/delete")
+      this.isLoading = false;
     }
   }
 };
 </script>
 
 <style scoped>
-.center {
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  border: 10px solid #ffffff;
-  padding: 10px;
-  margin: auto;
-  width: 60%;
-}
-#staffdashboard-inner {
-  padding: 80px;
-}
+#dashboard-inner {
 
-.btn {
-    margin: 15px auto;
 }
-
 </style>
