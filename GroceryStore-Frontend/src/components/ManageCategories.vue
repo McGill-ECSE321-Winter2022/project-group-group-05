fa0643832e20834235b9cd8121f68a74b911710d
jimcv <71361453+jimcv@users.noreply.github.com>
2022-04-02 00:42:14 -0400

    Select category and add/remove items

--- GroceryStore-Frontend/src/components/ManageCategories.vue
+++ GroceryStore-Frontend/src/components/ManageCategories.vue
@@ -1,12 +1,118 @@
 <!--Visibility: owner-->
 <!--Create/Delete categories, add/remove items to categories-->
 <!--View all items in a given category-->
-<template></template>
+<template>
+  <div id="categories-outer" v-if="isOwner">
+    <b-overlay :show="isLoading" rounded="sm">
+      <div :aria-hidden="isLoading ? 'true' : null" id="categories-inner">
+        <b-container fluid>
+          <b-row>
+            <b-col md="auto">
+              <StaffDashboard></StaffDashboard>
+            </b-col>
 
-<script>
-export default {
-  name: "ManageCategories",
-};
-</script>
+            <b-col>
+              <h1>Manage Item Categories</h1>
+              <div id="categories-control" class="mb-3">
+                <b-container fluid>
+                  <b-row>
+                    <b-col sm="2">
+                      <b-button block v-on:click="addDialog">Add Item</b-button>
+                    </b-col>
+                    <b-col sm="4">
+                      <b-form-select v-model="selectedCategory"
+                                :options="categoriesOptions"
+                                v-bind:disabled="isLoading"
+                                @input="atSelction">
+                        <template #first>
+                          <b-form-select-option :value="null" disabled>
+                            --- please select a category ---
+                          </b-form-select-option>
+                        </template>
+                      </b-form-select>
+                    </b-col>
+                    <b-col sm="3">
+                      <b-button block>Create New</b-button>
+                    </b-col>
+                    <b-col sm="3">
+                      <b-button block>Delete Category</b-button>
+                    </b-col>
+                  </b-row>
+                </b-container>
+              </div>
+
+              <div class="text-center h5" v-show="categoriesError">
+                <b style="color: red">{{ categoriesError }}</b>
+              </div>
+
+              <h3 class="mt-3">List of Items in Selected Category</h3>
+              <div id="selected-category-info">
+                <b-table
+                  id="category-item-table"
+                  hover
+                  fixed
+                  :items="itemsInSelectedCategory"
+                  :fields="itemFields"
+                  @row-clicked="removeDialog"
+                >
+                  <template #cell(name)="data">
+                      <b>{{ data.value }}</b>
+                  </template>
+                  <template #cell(price)="data">
+                    ${{ data.value | formatCurrency }}
+                  </template>
+                </b-table>
+
+              </div>
+
+            </b-col>
+          </b-row>
+        </b-container>
+
+        <b-modal
+          id="item-search"
+          title="Click to add item to this category"
+          hide-footer
+          scrollable
+        >
+          <b-input
+            v-model="itemSearchQuery"
+            placeholder="Search item name"
+            class="mb-2"
+          ></b-input>
+          <div class="text-center">
+            <b-table
+              hover
+              fixed
+              :items="filteredItemList"
+              :fields="itemFields"
+              @row-clicked="addConfirm"
+            >
+              <template #cell(price)="data">
+                ${{ data.value | formatCurrency }}
+              </template>
+            </b-table>
+          </div>
+        </b-modal>
+
+        <b-modal
+          id="remove-item-dialog"
+          title="Remove item"
+          hide-footer
+          centered>
+          <p class="mt-2 mb-2">
+            Confirm remove <b>{{ removeItem["name"]}}</b> from this category?
+          </p>
+          <div class="text-center">
+            <b-button variant="danger" v-on:click="removeConfirm" v-bind:disabled="isLoading">Confirm Remove</b-button>
+          </div>
+        </b-modal>
+
+      </div>
+    </b-overlay>
+  </div>
+</template>
+
+<script src="./ManageCategoriesScript.js"></script>
 
 <style scoped></style>
---
+++ GroceryStore-Frontend/src/components/ManageCategoriesScript.js
@@ -0,0 +1,141 @@
+import { AXIOS } from "../common/AxiosScript";
+import { LOGIN_STATE } from "../common/StateScript";
+import StaffDashboard from "./StaffDashboard";
+
+export default {
+  name: "ManageCategories",
+  components: { StaffDashboard },
+  data() {
+    return {
+      LOGIN_STATE,
+      isOwner: LOGIN_STATE.state.userType === "Owner",
+      isLoading: false,
+      categoriesError: "",
+      // browse categories
+      categoriesList: [],
+      categoriesOptions: [],
+      selectedCategory: null,
+      itemsInSelectedCategory: [],
+      // browse items in category
+      removeItem: "",
+      // browse items
+      itemList: [],
+      itemFields: [
+        {
+          key: "name",
+          sortable: true,
+        },
+        {
+          key: "price",
+          sortable: true,
+        },
+        {
+          key: "inventory",
+          sortable: true,
+        },
+      ],
+      itemSearchQuery: "",
+    };
+  },
+  computed: {
+    filteredItemList() {
+      return this.itemList.filter(item => {
+        return item["name"]
+          .toLowerCase()
+          .includes(this.itemSearchQuery.trim().toLowerCase());
+      });
+    },
+  },
+  created: async function() {
+    this.isLoading = true;
+    await this.fetchCategories();
+    await this.fetchItems();
+    this.updateSelection();
+    this.isLoading = false;
+  },
+  methods: {
+    atSelction: async function(value) {
+      this.categoriesError = "";
+      this.isLoading = true;
+      await this.fetchItemsInCategory(value);
+      this.isLoading = false;
+    },
+    removeDialog: function(item) {
+      this.removeItem = item;
+      this.$bvModal.show("remove-item-dialog");
+    },
+    removeConfirm: async function() {
+      this.categoriesError = "";
+      this.isLoading = true;
+      await this.removeItemFromCategory(this.selectedCategory, this.removeItem["name"]);
+      await this.fetchItemsInCategory(this.selectedCategory);
+      this.$bvModal.hide("remove-item-dialog");
+      this.isLoading = false;
+    },
+    addDialog: function() {
+      this.$bvModal.show("item-search");
+    },
+    addConfirm: async function(item) {
+      this.categoriesError = "";
+      this.isLoading = true;
+      await this.addItemToCategory(this.selectedCategory, item["name"]);
+      await this.fetchItemsInCategory(this.selectedCategory);
+      this.isLoading = false;
+    },
+    fetchCategories: function() {
+      return AXIOS.get("/itemCategory", {}).then(response => {
+        this.categoriesList = response.data;
+      }).catch(e => {
+        let errorMsg = e.response.data.message;
+        console.log(errorMsg);
+      });
+    },
+    fetchItems: function() {
+      return AXIOS.get("/item/getAll", {}).then(response => {
+        this.itemList = response.data;
+      }).catch(e => {
+        let errorMsg = e.response.data.message;
+        console.log(errorMsg);
+      });
+    },
+    fetchItemsInCategory: function(categoryName) {
+      return AXIOS.get("/itemCategory/".concat(categoryName).concat("/getItems"), {}).then(response => {
+        this.itemsInSelectedCategory = response.data;
+      }).catch(e => {
+        let errorMsg = e.response.data.message;
+        console.log(errorMsg);
+        this.categoriesError = errorMsg;
+      });
+    },
+    updateSelection: function() {
+      for (const category of this.categoriesList) {
+        let name = category["name"];
+        let option = { value: name, text: name};
+        this.categoriesOptions.push(option);
+      }
+    },
+    removeItemFromCategory:  function(categoryName, itemName) {
+      return AXIOS.patch("/itemCategory/".concat(categoryName).concat("/removeItem"), {}, {
+        params: {
+          itemName: itemName,
+        },
+      }).catch(e => {
+        let errorMsg = e.response.data.message;
+        console.log(errorMsg);
+        this.categoriesError = errorMsg;
+      });
+    },
+    addItemToCategory: function(categoryName, itemName) {
+      return AXIOS.patch("/itemCategory/".concat(categoryName).concat("/addItem"), {}, {
+        params: {
+          itemName: itemName,
+        },
+      }).catch(e => {
+        let errorMsg = e.response.data.message;
+        console.log(errorMsg);
+        this.categoriesError = errorMsg;
+      });
+    }
+  },
+};
+
