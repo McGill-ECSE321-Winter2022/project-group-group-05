fc36814ea28cda161094fdc0416d934691f4dae5
HwangPHS <56658501+HwangPHS@users.noreply.github.com>
2022-04-01 22:19:17 -0400

    quick implementation of openinghours
    a bit rough, but it'll do for now I think, so long as there aren't any bugs

--- GroceryStore-Frontend/src/components/ManageOpeningHours.vue
+++ GroceryStore-Frontend/src/components/ManageOpeningHours.vue
@@ -1,11 +1,98 @@
 <!--Visibility: owner-->
 <!--Edit opening hours-->
-<template></template>
-
-<script>
-export default {
-  name: "ManageOpeningHours",
-};
-</script>
-
-<style scoped></style>
+<template>
+  <b-container fluid>
+    <b-row class="vh-100" align-v="stretch">
+      <b-col><staff-dashboard></staff-dashboard></b-col>
+      <b-col cols="10">
+        <h1 class="header_style">Opening Hours</h1>
+        <b-alert show variant="danger" v-if="!isOwnerLoggedIn"
+          ><h4 class="alert-heading">Error:</h4>
+          You must be logged in as an Owner to access this page.
+        </b-alert>
+        <b-alert
+          :show="dismissCountDown"
+          dismissible
+          variant="danger"
+          @dismissed="dismissCountDown = 0"
+          @dismiss-count-down="countDownChanged"
+        >
+          Error: {{ errorMessage }}
+        </b-alert>
+        <div v-if="isOwnerLoggedIn">
+          <b-table
+            class="hours_table_style"
+            hover
+            fixed
+            :items="items"
+            :fields="fields"
+            @row-clicked="editOpeningHours"
+          >
+            <template #cell(startTime)="row">
+              {{ row.item.startTime | formatTime }}
+            </template>
+            <template #cell(endTime)="row">
+              {{ row.item.endTime | formatTime }}
+            </template>
+          </b-table>
+          <b-modal
+            id="editOpeningHours"
+            ref="modal"
+            :title="selectedWeekDay"
+            ok-title="Save"
+            @hidden="resetWeekdayForm"
+            @ok="handleOk"
+          >
+            <template>
+              <form ref="form" @submit.stop.prevent="updateOpeningHours">
+                <b-form-group
+                  label="Opening Time"
+                  label-for="opening-time-input"
+                  invalid-feedback="Opening Time must come before closing time"
+                  :state="isTimeValid"
+                  ><b-form-input
+                    id="opening-time-input"
+                    v-model="selectedStartTime"
+                    :state="isTimeValid"
+                    :type="'time'"
+                    required
+                  ></b-form-input>
+                </b-form-group>
+                <b-form-group
+                  label="Closing Time"
+                  label-for="closing-time-input"
+                  invalid-feedback="Opening Time must come before closing time"
+                  :state="isTimeValid"
+                  ><b-form-input
+                    id="closing-time-input"
+                    v-model="selectedEndTime"
+                    :state="isTimeValid"
+                    :type="'time'"
+                    required
+                  ></b-form-input>
+                </b-form-group>
+              </form>
+            </template>
+          </b-modal>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+<script src="./ManageOpeningHoursScript.js"></script>
+<style scoped>
+/* Styling for Page Title */
+.header_style {
+  padding-top: 15px;
+  padding-left: 100px;
+  border-color: #0d6efd;
+  border-style: solid;
+  border-width: 0px 0px 6px 0px;
+  text-align: left;
+  margin-bottom: 30px;
+}
+.hours_table_style {
+  width: 95%;
+  margin: auto;
+}
+</style>
---
+++ GroceryStore-Frontend/src/components/ManageOpeningHoursScript.js
@@ -0,0 +1,150 @@
+import StaffDashboard from "./StaffDashboard";
+import { AXIOS } from "../common/AxiosScript";
+import { LOGIN_STATE } from "../common/StateScript";
+import moment from "moment";
+
+async function fetchWeekday(dayOfWeek) {
+  return AXIOS.get("openingH/".concat(dayOfWeek))
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      AXIOS.post(
+        "openingH/".concat(dayOfWeek),
+        {},
+        {
+          params: {
+            startH: "09:00",
+            endH: "17:00",
+          },
+        }
+      )
+        .then(response => {
+          return response.data;
+        })
+        .catch(error => {
+          // This error should never occur. POST Requests will only throw an error
+          // when the instance we are creating already exists. This is never the case
+          // here because this code will only run if the instance does not exist.
+          console.log(error.response.data.message);
+        });
+    });
+}
+
+export default {
+  name: "ManageOpeningHours",
+  components: {
+    StaffDashboard,
+  },
+  created: async function () {
+    this.isOwnerLoggedIn =
+      LOGIN_STATE.state.isLoggedIn && LOGIN_STATE.state.userType === "Owner";
+    /**
+     * There should always be once instance of OpeningHours in the database for every day of the week.
+     *
+     * We fetch each instance and load them into the items list ordered by day of the week. If a day
+     * does not exist in the database, we will create it.
+     */
+    this.items.push(await fetchWeekday("Sunday"));
+    this.items.push(await fetchWeekday("Monday"));
+    this.items.push(await fetchWeekday("Tuesday"));
+    this.items.push(await fetchWeekday("Wednesday"));
+    this.items.push(await fetchWeekday("Thursday"));
+    this.items.push(await fetchWeekday("Friday"));
+    this.items.push(await fetchWeekday("Saturday"));
+  },
+  data() {
+    return {
+      isOwnerLoggedIn: false,
+      items: [],
+      fields: [
+        {
+          key: "daysOfWeek",
+          label: "Day of the Week",
+          thStyle: { width: "60%" },
+        },
+        { key: "startTime", label: "Opens at", thStyle: { width: "20%" } },
+        { key: "endTime", label: "Closes at", thStyle: { width: "20%" } },
+      ],
+
+      // Used for error message
+      dismissSecs: 5,
+      dismissCountDown: 0,
+      errorMessage: "",
+
+      // form data
+      selectedWeekDayIndex: 0,
+      selectedWeekDay: "",
+      selectedStartTime: "",
+      selectedEndTime: "",
+      editAttempt: false,
+    };
+  },
+  computed: {
+    isTimeValid: function () {
+      if (
+        this.selectedStartTime !==
+          this.items[this.selectedWeekDayIndex].startTime ||
+        this.selectedEndTime !== this.items[this.selectedWeekDayIndex].endTime
+      ) {
+        this.editAttempt = true;
+      }
+      return this.editAttempt
+        ? moment(this.selectedEndTime, "HH:mm").isAfter(
+            moment(this.selectedStartTime, "HH:mm")
+          )
+        : null;
+    },
+  },
+  methods: {
+    countDownChanged(dismissCountDown) {
+      this.dismissCountDown = dismissCountDown;
+    },
+    resetWeekdayForm() {
+      this.selectedWeekDayIndex = 0;
+      this.selectedWeekDay = "";
+      this.selectedStartTime = "";
+      this.selectedEndTime = "";
+      this.editAttempt = false;
+    },
+    handleOk(okEvent) {
+      // Prevent the default function of the "ok" button in b-modal and replace with custom function
+      okEvent.preventDefault();
+      this.updateOpeningHours();
+    },
+    editOpeningHours(dayOfWeek, rowIndex) {
+      this.selectedWeekDayIndex = rowIndex;
+      this.selectedWeekDay = dayOfWeek.daysOfWeek;
+      this.selectedStartTime = dayOfWeek.startTime;
+      this.selectedEndTime = dayOfWeek.endTime;
+      this.editAttempt = false;
+      this.$bvModal.show("editOpeningHours");
+    },
+    async updateOpeningHours() {
+      this.editAttempt = true;
+      await AXIOS.patch(
+        "/openingH/".concat(this.selectedWeekDay),
+        {},
+        {
+          params: {
+            startH: moment(this.selectedStartTime, "HH:mm:ss").format("HH:mm"),
+            endH: moment(this.selectedEndTime, "HH:mm:ss").format("HH:mm"),
+          },
+        }
+      )
+        .then(response => {
+          this.items[this.selectedWeekDayIndex].startTime =
+            response.data.startTime;
+          this.items[this.selectedWeekDayIndex].endTime = response.data.endTime;
+        })
+        .catch(error => {
+          console.log(error.response.data.message);
+          this.dismissCountDown = this.dismissSecs;
+          this.errorMessage = error.response.data.message;
+        });
+      this.$nextTick(() => {
+        this.$bvModal.hide("editOpeningHours");
+      });
+    },
+  },
+};
