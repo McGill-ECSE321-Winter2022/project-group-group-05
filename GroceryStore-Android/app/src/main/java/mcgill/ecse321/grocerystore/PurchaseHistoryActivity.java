64cf5b6e687cec6d7285b99151b0c818a1ee4026
PeiniCheng <peini.cheng@mail.mcgill.ca>
2022-04-06 22:21:24 -0400

    change layout

--- GroceryStore-Android/app/src/main/java/mcgill/ecse321/grocerystore/PurchaseHistoryActivity.java
+++ GroceryStore-Android/app/src/main/java/mcgill/ecse321/grocerystore/PurchaseHistoryActivity.java
@@ -8,6 +8,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 public class PurchaseHistoryActivity extends AppCompatActivity {
     public DrawerLayout drawerLayout;
@@ -17,39 +18,9 @@ public class PurchaseHistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_purchase_history);
-        drawerLayout = findViewById(R.id.customer_drawer_history);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
-
-        // pass the Open and Close toggle for the drawer layout listener
-        // to toggle the button
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
-
-        // to make the Navigation drawer icon always appear on the action bar
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void logout(MenuItem item) {
-        Intent mainPage = new Intent(this, MainActivity.class);
+    public void main(View v) {
+        Intent mainPage = new Intent(this, CustomerMainActivity.class);
         startActivity(mainPage);
-    }
-
-    public void history(MenuItem item) {
-        Intent historyPage = new Intent(this, PurchaseHistoryActivity.class);
-        startActivity(historyPage);
-    }
-
-    public void profile(MenuItem item) {
-        Intent profilePage = new Intent(this, CustomerProfileActivity.class);
-        startActivity(profilePage);
     }
 }
